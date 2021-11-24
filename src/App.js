12be50d9d9f4486784ebc4d497bbfe143c0e37a8
damildrizzy <damiloladrizzy@yahoo.com>
2021-11-24 08:26:48 +0100

    see if we can access user account

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-// const OPENSEA_LINK = '';
-// const TOTAL_MINT_COUNT = 50;
+const OPENSEA_LINK = '';
+const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
 
-    const checkIfWalletIsConnected = () => {
     /*
-    * First make sure we have access to window.ethereum
+    * Just a state variable we use to store our user's public wallet. Don't forget to import useState.
     */
-    const { ethereum } = window;
+    const [currentAccount, setCurrentAccount] = useState("");
+    
+    /*
+    * Gotta make sure this is async.
+    */
+    const checkIfWalletIsConnected = async () => {
+      const { ethereum } = window;
 
-    if (!ethereum) {
-      console.log("Make sure you have metamask!");
-      return;
-    } else {
-      console.log("We have the ethereum object", ethereum);
-    }
+      if (!ethereum) {
+          console.log("Make sure you have metamask!");
+          return;
+      } else {
+          console.log("We have the ethereum object", ethereum);
+      }
+
+      /*
+      * Check if we're authorized to access the user's wallet
+      */
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+      /*
+      * User can have multiple authorized accounts, we grab the first one if its there!
+      */
+      if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log("Found an authorized account:", account);
+          setCurrentAccount(account)
+      } else {
+          console.log("No authorized account found")
+      }
   }
 
   // Render Methods
@@ -31,22 +52,18 @@ const App = () => {
     </button>
   );
 
-  /*
-  * This runs our function when the page loads.
-  */
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
 
   return (
-    <div className="App">
+     <div className="App">
       <div className="container">
         <div className="header-container">
           <p className="header gradient-text">My NFT Collection</p>
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {/* Add your render method here */}
           {renderNotConnectedContainer()}
         </div>
         <div className="footer-container">

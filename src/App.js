1e5d271465cf9d7c9c6c5f9b1b3de539bc0938ce
damildrizzy <damiloladrizzy@yahoo.com>
2021-11-24 08:27:37 +0100

    connect wallet button

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import React, { useEffect, useState } from "react";
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
-// Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = '';
-const TOTAL_MINT_COUNT = 50;
+// const OPENSEA_LINK = '';
+// const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
 
-    /*
-    * Just a state variable we use to store our user's public wallet. Don't forget to import useState.
-    */
     const [currentAccount, setCurrentAccount] = useState("");
     
-    /*
-    * Gotta make sure this is async.
-    */
     const checkIfWalletIsConnected = async () => {
       const { ethereum } = window;
 
@@ -28,14 +21,8 @@ const App = () => {
           console.log("We have the ethereum object", ethereum);
       }
 
-      /*
-      * Check if we're authorized to access the user's wallet
-      */
       const accounts = await ethereum.request({ method: 'eth_accounts' });
 
-      /*
-      * User can have multiple authorized accounts, we grab the first one if its there!
-      */
       if (accounts.length !== 0) {
           const account = accounts[0];
           console.log("Found an authorized account:", account);
@@ -45,9 +32,36 @@ const App = () => {
       }
   }
 
+  /*
+  * Implement your connectWallet method here
+  */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      /*
+      * Fancy method to request access to account.
+      */
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      /*
+      * Boom! This should print out public address once we authorize Metamask.
+      */
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]); 
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   // Render Methods
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect to Wallet
     </button>
   );
@@ -56,15 +70,24 @@ const App = () => {
     checkIfWalletIsConnected();
   }, [])
 
+  /*
+  * Added a conditional render! We don't want to show Connect to Wallet if we're already conencted :).
+  */
   return (
-     <div className="App">
+    <div className="App">
       <div className="container">
         <div className="header-container">
           <p className="header gradient-text">My NFT Collection</p>
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount === "" ? (
+            renderNotConnectedContainer()
+          ) : (
+            <button onClick={null} className="cta-button connect-wallet-button">
+              Mint NFT
+            </button>
+          )}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
